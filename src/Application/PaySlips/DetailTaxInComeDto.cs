6634ebf9c8df67f0de4787b6ed57e25308d8ec1a
tcminh1108 <tcminh1108@gmail.com>
2023-05-29 10:59:12 +0700

    Update Total Salary

--- src/Application/PaySlips/DetailTaxInComeDto.cs
+++ src/Application/PaySlips/DetailTaxInComeDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using hrOT.Application.Common.Mappings;
 using hrOT.Domain.Entities;
 
-namespace hrOT.Application.PaySlips.Commands;
+namespace hrOT.Application.PaySlips;
 public class DetailTaxInComeDto : IMapFrom<DetailTaxIncome>
 {
     public int? Level { get; set; }
