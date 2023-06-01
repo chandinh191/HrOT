04ae18915c04bfc0644b4db8164e74e9d3a47477
tcminh1108 <tcminh1108@gmail.com>
2023-06-01 08:59:26 +0700

    Fix bug get Family

--- src/Application/Families/FamilyDto.cs
+++ src/Application/Families/FamilyDto.cs
@@ -5,16 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using hrOT.Application.Common.Mappings;
 using hrOT.Domain.Entities;
+using hrOT.Domain.Enums;
 
 namespace hrOT.Application.Families;
 public class FamilyDto : IMapFrom<Family>
 {
-    public Guid Id { get; set; }
     public Guid EmployeeId { get; set; }
-    public string? FatherName { get; set; }
-    public string? MotherName { get; set; }
-    public int? NumberOfDependents { get; set; }
+    public string? Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public Relationship Relationship { get; set; }
     public string? HomeTown { get; set; }
-    public bool IsDeleted { get; set; }
-    public virtual Employee Employee { get; set; }
 }
