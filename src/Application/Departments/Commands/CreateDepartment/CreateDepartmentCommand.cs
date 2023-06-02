6734b3e405de98eb674f3a0913ec8044ab7c04a2
tcminh1108 <tcminh1108@gmail.com>
2023-06-02 10:25:13 +0700

    fix create department

--- src/Application/Departments/Commands/CreateDepartment/CreateDepartmentCommand.cs
+++ src/Application/Departments/Commands/CreateDepartment/CreateDepartmentCommand.cs
@@ -11,8 +11,6 @@ using MediatR;
 namespace hrOT.Application.Departments.Commands.CreateDepartment;
 public record CreateDepartmentCommand : IRequest<Guid>
 {
-    public Guid PositionId { get; set; }
-    public Guid EmployeeId { get; set; }
     public string? Name { get; init; }
     public string? Description { get; init; }
 }
@@ -29,8 +27,6 @@ public class CreateDepartmentCommandHandler : IRequestHandler<CreateDepartmentCo
     public async Task<Guid> Handle(CreateDepartmentCommand request, CancellationToken cancellationToken)
     {
         var entity = new Department();
-        //entity.PositionId = request.PositionId;
-        //entity.EmployeeId = request.EmployeeId;   
         entity.Name = request.Name;
         entity.Description = request.Description;
 
--- src/Application/Departments/Commands/UpdateDepartment/UpdateDepartmentCommand.cs
+++ src/Application/Departments/Commands/UpdateDepartment/UpdateDepartmentCommand.cs
@@ -13,8 +13,6 @@ namespace hrOT.Application.Departments.Commands.UpdateDepartment;
 public record UpdateDepartmentCommand : IRequest<string>
 {
     public Guid Id { get; init; }
-    public Guid PositionId { get; set; }
-    public Guid EmployeeId { get; set; }
     public string? Name { get; init; }
     public string? Description { get; init; }
 }
